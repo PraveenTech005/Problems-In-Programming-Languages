97fcdfda0e34742ea0467a97dab3ebdf4c4f598c
PRAVEEN <92732706+PraveenTech005@users.noreply.github.com>
2023-01-12 21:08:48 +0530

    Update 2_UserInput.c

--- C Language/2_UserInput.c
+++ C Language/2_UserInput.c
@@ -5,10 +5,10 @@ int main()
     char Uname[50];
     printf("Enter Your First Name : \n");                           //It Only Reads Until You Press Space
     scanf("%s\n",&Uname);                                         //If You Type - For Ex.("Praveen Tech") 
-    printf("Hello, %s. Welcome To C Language Problems\n", Uname); //It Takes "Praveen" As Input
+    printf("Hello, %s. Welcome To C Repository\n", Uname); //It Takes "Praveen" As Input
 
     printf("Enter Your Full Name : \n");                          //It Reads Until You Press Enter
     fflush(stdin);                                                //If You Type - For Ex.("Praveen Tech")
     gets(Uname);                                                  //It Takes "Praveen Tech" (Full Text) As Input
-    printf("Hello, %s. Welcome To This C Language Problems Solving Repository\n",Uname);  //Here fflush() For Clear Garbage(random) Values
+    printf("Hello, %s. Welcome To C Repository\n",Uname);  //Here fflush() For Clear Garbage(random) Values
 }                                                                 //gets() For Read Input
