dbe065e49ff6f779582988cbc620f62faa77ce8a
PRAVEEN <92732706+PraveenTech005@users.noreply.github.com>
2023-01-12 21:10:38 +0530

    Update 5_User_Defined_Tables.c

--- C Language/5_User_Defined_Tables.c
+++ C Language/5_User_Defined_Tables.c
@@ -5,13 +5,9 @@ int main()
     int t, n, i;
     printf("\nEnter A Table You Want : ");
     scanf("%d",&t);
-    printf("\nHow Much Number You Want : ");
+    printf("\nNumber Upto : ");
     scanf("%d",&n);
 
     for(i=1;i<=n;i++)
-    {
         printf("\n%d X %d = %d",t, i, t*i);
-    }
-    
-
 }
